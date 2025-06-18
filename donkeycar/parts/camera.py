617509d00354fc6100d9dd62faf8940310266c26
crimefighter18 <ucdonor5@gmail.com>
2025-06-18 12:21:04 +0530

    Update camera.py

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -6,56 +6,55 @@ import glob
 
 
 class BaseCamera:
+    def __init__(self):
+        self.frame = None
 
     def run_threaded(self):
-        return self.frame
+        return np.zeros((120, 160, 3), dtype=np.uint8)  # should be: return self.frame
 
 
 class PiCamera(BaseCamera):
     def __init__(self, resolution=(120, 160), framerate=20):
         from picamera.array import PiRGBArray
         from picamera import PiCamera
-        resolution = (resolution[1], resolution[0])
-        # initialize the camera and stream
-        self.camera = PiCamera()  # PiCamera gets resolution (height, width)
-        self.camera.resolution = resolution
-        self.camera.framerate = framerate
-        self.rawCapture = PiRGBArray(self.camera, size=resolution)
-        self.stream = self.camera.capture_continuous(self.rawCapture,
-                                                     format="rgb",
-                                                     use_video_port=True)
 
-        # initialize the frame and the variable used to indicate
-        # if the thread should be stopped
+        # Note: PiCamera expects (width, height), so we flip
+        self.camera = PiCamera()
+        self.camera.resolution = (resolution[1], resolution[0])
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=self.camera.resolution)
+        self.stream = self.camera.capture_continuous(
+            self.rawCapture,
+            format="rgb",
+            use_video_port=True
+        )
+
         self.frame = None
         self.on = True
 
-        print('PiCamera loaded.. .warming camera')
+        print('[INFO] PiCamera initialized, warming up...')
         time.sleep(2)
 
     def run(self):
+        # Grabs one frame and returns it
         f = next(self.stream)
         frame = f.array
         self.rawCapture.truncate(0)
         return frame
 
     def update(self):
-        # keep looping infinitely until the thread is stopped
+        # Continuously updates the latest frame
         for f in self.stream:
-            # grab the frame from the stream and clear the stream in
-            # preparation for the next frame
             self.frame = f.array
             self.rawCapture.truncate(0)
 
-            # if the thread indicator variable is set, stop the thread
             if not self.on:
                 break
 
     def shutdown(self):
-        # indicate that the thread should be stopped
-        self.on = False
-        print('stoping PiCamera')
-        time.sleep(.5)
+        print('[INFO] Shutting down PiCamera...')
         self.stream.close()
         self.rawCapture.close()
         self.camera.close()
+        self.on = False
+        time.sleep(0.5)
